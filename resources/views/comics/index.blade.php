540c52c537f4c1dd7f2391764ae31e49bb5c992b
GiPisano <pisanogiacomo9@gmail.com>
2024-03-25 18:56:45 +0100

    add show.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,28 +7,30 @@
     <div class="container py-4">
       <h1>Comics list</h1>
 
-      <table class="tale">
+      <table class="table">
         <thead>
           <th>ID</th>
           <th>title</th>
-          <th>description</th>
-          <th>thumb</th>
           <th>price</th>
           <th>series</th>
           <th>sale_date</th>
           <th>type</th>
+          <th>more</th>
         </thead>
         <tbody>
           @foreach($comics as $comic)
           <tr>
             <td>{{ $comic->id }}</td>
             <td>{{ $comic->title }}</td>
-            <td>{{ $comic->description }}</td>
-            <td>{{ $comic->thumb }}</td>
             <td>{{ $comic->price }}</td>
             <td>{{ $comic->series }}</td>
             <td>{{ $comic->sale_date }}</td>
             <td>{{ $comic->type }}</td>
+            <td>
+              <a href="{{ route('comics.show', $comic->id) }}">
+                link
+              </a>
+            </td>
           </tr>
           @endforeach
         </tbody>
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('title', 'comics')
+
+@section('main-content')
+
+   
+    <div class="container">
+        <h1>Comic description</h1>
+            <a href="{{ route('comics.index')}}" class="btn btn-primary mb-3">torna alla lista</a>
+        <div class="row g-5">
+            <div class="col-4">
+                <img src="{{ $comic->thumb }}" alt="">
+            </div>
+            <div class="col-8">
+                <h2 class="h4">Description</h2>
+                <p>{{ $comic->description }}</p>
+            
+            <div class="row">
+                <div class="col-6">
+                    <h2 class="h4">Price</h2>
+                    <p>{{ $comic->price }}</p>
+                </div>
+                <div class="col-6">
+                    <h2 class="h4">Sale date</h2>
+                    <p>{{ $comic->sale_date }}</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    
+@endsection
